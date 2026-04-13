6234c5d41b0804073b09b75900c75bb82ba9eb77
Kevin Balicot <kevinbalicot+github@proton.me>
2026-04-13 15:35:00 +0200

    fix: remove services/** glob import causing missing file error on install

--- config/services.php
+++ config/services.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import('services/**');
-
     $services = $containerConfigurator->services();
 
     $services->defaults()
